21d7d9a483ea0e6e067be8c92765b62b46dd02b1
elbroandrew <elbroandrew@gmail.com>
2022-11-22 13:03:59 +1000

    feat: add remove all button, add text from field input

--- script.js
+++ script.js
@@ -15,17 +15,24 @@ function toggle_class(element, from, to){
 
 update_checkboxes(checkboxes);
 
-const todo_list = document.querySelector(".todo-container__list");
+const todo_list =  document.querySelector(".todo-container__list");
 
 
 function remove_todo(button_pressed){
     todo_list.removeChild(button_pressed.parentNode);
 }
 
+function remove_all(){
+    let todo_list = document.querySelector(".todo-container__list");
+    for(let i = 0; i < todo_list.children.length; ){
+        todo_list.removeChild(todo_list.children[i])
+    }
+}
+
 function create_todo() {
     const li = document.createElement('li');
     const span_text = document.createElement("span");
-    span_text.innerText = `text${todo_list.children.length + 1}`;
+    span_text.innerText = `${document.querySelector('.input-block__input-elem').value}`;
     span_text.classList.add("todo-container__text", "todo-container__text_state_normal");
     const input_checkbox = document.createElement("input");
     input_checkbox.type = "checkbox";
