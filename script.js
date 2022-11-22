a093366535e206f1d15e3960d9a770bbd02a95a8
elbroandrew <elbroandrew@gmail.com>
2022-11-22 10:33:36 +1000

    refactor: toggle strikethrough

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 
-let checkbox_list = Array.from(document.getElementsByClassName("todo-container__checkbox"))
-checkbox_list.forEach( c => {
-    c.toggle_class = function(element, from, to){
-        element.classList.remove(from);
-        element.classList.add(to);
-    }
-})
-
 
 function pprint(checkbox){
     let text = checkbox.parentNode.querySelector(".todo-container__text");
     if (checkbox.checked){
-        checkbox.toggle_class(text, "todo-container__text_state_normal", "todo-container__text_state_strikethrough");
-
+        toggle_strikethrough(text, "todo-container__text_state_normal", "todo-container__text_state_strikethrough");
     }else{
-        checkbox.toggle_class(text, "todo-container__text_state_strikethrough", "todo-container__text_state_normal")
+        toggle_strikethrough(text, "todo-container__text_state_strikethrough", "todo-container__text_state_normal")
     }
 }
 
+function toggle_strikethrough(element, from, to){
+    element.classList.remove(from);
+    element.classList.add(to);
+}
