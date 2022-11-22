a5eccd1e0e041d31d9c8931e3ba100379bfefd1e
elbroandrew <elbroandrew@gmail.com>
2022-11-22 10:38:06 +1000

    feat: add toggle method to button

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
+let checkbox_list = Array.from(document.getElementsByClassName("todo-container__checkbox"))
+checkbox_list.forEach( c => {
+    c.toggle_class = function(element, from, to){
+        element.classList.remove(from);
+        element.classList.add(to);
+    }
+});
 
-function pprint(checkbox){
+
+function change_todo_state(checkbox){
     let text = checkbox.parentNode.querySelector(".todo-container__text");
+    const remove_button = checkbox.parentNode.querySelector(".todo-container__button");
+
     if (checkbox.checked){
-        toggle_strikethrough(text, "todo-container__text_state_normal", "todo-container__text_state_strikethrough");
+        checkbox.toggle_class(text, "todo-container__text_state_normal", "todo-container__text_state_strikethrough");
+        checkbox.toggle_class(remove_button, "todo-container__button_state_active", "todo-container__button_state_disabled")
     }else{
-        toggle_strikethrough(text, "todo-container__text_state_strikethrough", "todo-container__text_state_normal")
+        checkbox.toggle_class(text, "todo-container__text_state_strikethrough", "todo-container__text_state_normal")
     }
 }
 
-function toggle_strikethrough(element, from, to){
-    element.classList.remove(from);
-    element.classList.add(to);
-}
