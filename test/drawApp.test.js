67350556caf269b4ab60650ed61eebdff3b4a7d0
Andrew <elbroandrew@gmail.com>
2023-02-07 18:39:46 +1000

    first test to check drawApp html elements

--- test/drawApp.test.js
+++ test/drawApp.test.js
@@ -1,8 +1,10 @@
-// import { drawApp } from "../src/drawApp.js";
 const { drawApp } = require("../src/drawApp");
+const { JSDOM } = require("jsdom");
+let expect = require('chai').expect;
 
 describe("drawApp", () => {
     let el;
+    const document = (new JSDOM(`...`)).window.document;
 
     beforeEach(() => {
         el = document.createElement('div');
@@ -11,5 +13,10 @@ describe("drawApp", () => {
 
     it('is a function', function () {
         expect(drawApp).to.be.instanceof(Function);
+    });
+
+    it("draws basic markup", () => {
+        expect(el.querySelector("h1")).to.be.ok;
+        expect(el.querySelector("button")).to.be.ok;
     });
 })
