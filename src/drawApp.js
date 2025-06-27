192fe520400ef6a6a5dca34b0684b99e4b173104
Andrew <elbroandrew@gmail.com>
2025-06-27 20:16:43 +1000

    fix: отображение иконок

--- src/drawApp.js
+++ src/drawApp.js
@@ -11,18 +11,26 @@ export function drawApp(el) {
   el.innerHTML = `
     <div class="header">
       <h1>ToDo App</h1>
-      <button id="logoutBtn">Logout</button>
+      <button id="logoutBtn">
+      <i class="fas fa-sign-out-alt"></i>Выход
+      </button>
     </div>
     <div class="input-block">
       <input type="text" class="input-block__input-elem" placeholder="Add new task">
-      <button class="input-block__add-button-elem">Add Task</button>
+      <button class="input-block__add-button-elem">
+        <i class="fas fa-plus"></i>Создать задачу
+      </button>
     </div>
     <div class="todo-container">
       <ul class="todo-container__list"></ul>
     </div>
     <div class="actions-block">
-      <button class="remove-selected-todos-button">Remove Selected</button>
-      <button class="remove-todos-button">Remove All</button>
+      <button class="remove-selected-todos-button">
+        <i class="fas fa-trash-alt"></i> Удалить выбранные
+      </button>
+      <button class="remove-todos-button">
+        <i class="fas fa-broom"></i> Удалить все
+      </button>
     </div>
   `;
 
